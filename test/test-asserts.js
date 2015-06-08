96878e91b2d3ddc419c45b0d21ed3d89eb5215ae
Sergey Fedosenko <superpslam@gmail.com>
2015-06-08 11:49:23 +0300

    #2 Enum constants usage within test-asserts

--- test/test-asserts.js
+++ test/test-asserts.js
@@ -1,5 +1,7 @@
 
 var assert = require('assert');
+var Group = require('./../lib/compute-services/domain/group.js');
+var DataCenter = require('./../lib/compute-services/domain/datacenter.js');
 
 module.exports = TestAsserts;
 
@@ -7,11 +9,11 @@ function TestAsserts () {
     var self = this;
 
     self.assertThatDataCenterIsDe1 = function (dataCenter) {
-        assert.equal(dataCenter.id, "de1");
-        assert.equal(dataCenter.name, "DE1 - Germany (Frankfurt)");
+        assert.equal(dataCenter.id, DataCenter.DE_FRANKFURT.id);
+        assert.equal(dataCenter.name, DataCenter.DE_FRANKFURT.name);
     };
 
     self.assertThatGroupIsDefault = function (group) {
-        assert.equal(group.name, "Default Group");
+        assert.equal(group.name, Group.DEFAULT);
     };
 }
