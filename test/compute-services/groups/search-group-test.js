edd6b151e42345194d586f2996f1fbc226a7a247
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-06-22 20:54:32 +0300

    #65 Design, Development and Prototyping of SDK architecture

--- test/compute-services/groups/search-group-test.js
+++ test/compute-services/groups/search-group-test.js
@@ -1,12 +1,14 @@
+
+var vcr = require('nock-vcr-recorder-mocha');
 var Sdk = require('./../../../lib/clc-sdk.js');
 var compute = new Sdk().computeServices();
 var TestAsserts = require("./../../test-asserts.js");
 
-describe('Search group by name and datacenter [INTEGRATION]', function () {
+vcr.describe('Search group by name and datacenter [UNIT]', function () {
     var assertThatGroupIsDefault = TestAsserts.assertThatGroupIsDefault;
 
-    it('Should found "Default group"', function (done) {
-        this.timeout(10000);
+    it('Should found Default group', function (done) {
+        this.timeout(100000);
 
         var DataCenter = compute.DataCenter;
         var Group = compute.Group;
