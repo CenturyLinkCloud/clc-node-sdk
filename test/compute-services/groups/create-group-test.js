0a91937b33d664e3cfa32c76cc26225b18113442
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-06-24 15:55:05 +0300

    #40 Implement possibilities to create group

---
+++ test/compute-services/groups/create-group-test.js
@@ -0,0 +1,23 @@
+
+var _ = require('underscore');
+var vcr = require('nock-vcr-recorder-mocha');
+var Sdk = require('./../../../lib/clc-sdk.js');
+var compute = new Sdk('cloud_user', 'cloud_user_password').computeServices();
+
+
+vcr.describe('Create Group Operation [UNIT]', function () {
+    it('Should create Group1 in DE1 DataCenter', function (done) {
+        this.timeout(10000);
+
+        compute
+            .groups()
+            .create({
+                parentGroupId: 'e9cf5a7a9fad43a8a9184d0265ae076c',
+                name: 'Group1',
+                description: 'Test Group'
+            })
+            .then(function () {
+                done();
+            });
+    });
+});
