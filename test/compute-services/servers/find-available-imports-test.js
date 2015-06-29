3e91b4990b8b39b6ae489556e23b657eeedbfe5b
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-06-29 17:08:40 +0300

    #65 Design, Development and Prototyping of SDK architecture

--- test/compute-services/servers/find-available-imports-test.js
+++ test/compute-services/servers/find-available-imports-test.js
@@ -4,13 +4,13 @@ var TestAsserts = require("./../../test-asserts.js");
 
 describe('Find available server imports [UNIT]', function () {
     var compute = new Sdk().computeServices();
-    var service = compute.servers();
+    var templates = compute.templates();
     var DataCenter = compute.DataCenter;
 
     vcr.it('Should found list of server imports', function (done) {
         this.timeout(50 * 1000);
 
-        service
+        templates
             .findAvailableServerImports(DataCenter.DE_FRANKFURT)
             .then(TestAsserts.assertNotNull)
             .then(function () {
