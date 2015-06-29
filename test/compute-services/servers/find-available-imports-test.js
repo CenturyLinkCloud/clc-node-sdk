c3fe9d00e7f74a59698751e8b0432b279ba032fb
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-06-29 16:42:51 +0300

    #65 Design, Development and Prototyping of SDK architecture

--- test/compute-services/servers/find-available-imports-test.js
+++ test/compute-services/servers/find-available-imports-test.js
@@ -1,15 +1,16 @@
 var Sdk = require('./../../../lib/clc-sdk.js');
+var vcr = require('nock-vcr-recorder-mocha');
 var TestAsserts = require("./../../test-asserts.js");
 
-describe('Find available server imports [INTEGRATION]', function () {
+describe('Find available server imports [UNIT]', function () {
 
     var compute = new Sdk().computeServices();
     var service = compute.servers();
     var DataCenter = compute.DataCenter;
 
 
-    it('Should found list of server imports', function (done) {
-        this.timeout(10 * 5000);
+    vcr.it('Should found list of server imports', function (done) {
+        this.timeout(50 * 1000);
 
         service
             .findAvailableServerImports(DataCenter.DE_FRANKFURT)
