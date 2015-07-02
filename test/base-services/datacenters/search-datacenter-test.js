f5859e7a0eb3c0234fe15ba628c0efd49a79a270
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-07-02 14:20:40 +0300

    add more tests for data centers

--- test/base-services/datacenters/search-datacenter-test.js
+++ test/base-services/datacenters/search-datacenter-test.js
@@ -1,9 +1,10 @@
 var vcr = require('nock-vcr-recorder-mocha');
 
 var Sdk = require('./../../../lib/clc-sdk.js');
-var compute = new Sdk().baseServices();
+var compute = new Sdk('cloud_user', 'cloud_user_password').baseServices();
 var TestAsserts = require("./../../test-asserts.js");
 var assert = require('assert');
+var _ = require('underscore');
 
 vcr.describe('Search datacenter operation [UNIT]', function () {
     var assertThatDataCenterIsDe1 = TestAsserts.assertThatDataCenterIsDe1;
@@ -21,9 +22,7 @@ vcr.describe('Search datacenter operation [UNIT]', function () {
                 }
             })
             .then(assertThatDataCenterIsDe1)
-            .then(function () {
-                done();
-            });
+            .then(done);
     });
 
     it('Should found ca1 datacenter by conditional criteria', function (done) {
@@ -43,9 +42,34 @@ vcr.describe('Search datacenter operation [UNIT]', function () {
                 assert.equal(result.length, 1);
                 assert.equal(result[0].id, 'ca1');
             })
-            .then(function () {
-                done();
-            });
+            .then(done);
+    });
+
+    it('Should found de1, ca1 datacenters by conditional criteria', function (done) {
+        this.timeout(10000);
+
+        var criteria = {
+            or: [
+                {
+                    and: [
+                        { nameContains: 'CA' },
+                        { name: 'CA1 - Canada (Vancouver)' },
+                        { and: [{ nameContains: 'Canada' }, { id: 'ca1' }] }
+                    ]
+                },
+                {id: 'de1'}
+            ]
+        };
+
+        compute.dataCenters()
+            .find(criteria)
+            .then(function(result) {
+                assert.equal(result.length, 2);
+                var ids = _.pluck(result, 'id');
+                assert.equal(ids.indexOf('ca1') > -1, true);
+                assert.equal(ids.indexOf('de1') > -1, true);
+            })
+            .then(done);
     });
 
     it('Should not found any data center', function (done) {
@@ -61,9 +85,7 @@ vcr.describe('Search datacenter operation [UNIT]', function () {
         compute.dataCenters()
             .find(criteria)
             .then(assertThatArrayIsEmpty)
-            .then(function () {
-                done();
-            });
+            .then(done);
     });
 
     it('Should found all data centers', function (done) {
@@ -72,8 +94,8 @@ vcr.describe('Search datacenter operation [UNIT]', function () {
         compute.dataCenters()
             .find()
             .then(function (result) {
-                assert.equal(result.length > 0, true);
-                done();
-            });
+                assert.equal(result.length, 13);
+            })
+            .then(done);
     });
 });
