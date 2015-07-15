a87ebd2ed84f4d6c91821fbcce22cfa22e28e4d2
Sergey Fedosenko <superpslam@gmail.com>
2015-07-15 17:03:32 +0300

    #61 Implement possibilities to get group billing stats

--- test/compute-services/groups/get-billing-stats.js
+++ test/compute-services/groups/get-billing-stats.js
@@ -1,14 +1,10 @@
 
 var vcr = require('nock-vcr-recorder-mocha');
 var Sdk = require('./../../../lib/clc-sdk.js');
-var compute = new Sdk().computeServices();
-var TestAsserts = require("./../../test-asserts.js");
+var compute = new Sdk('cloud_user', 'cloud_user_password').computeServices();
 var assert = require('assert');
-var _ = require("underscore");
 
 vcr.describe('Get group billing stats [UNIT]', function () {
-    var DataCenter = compute.DataCenter;
-    var Group = compute.Group;
 
     it('Should return stats for Default group in DE1', function (done) {
         this.timeout(10000);
@@ -16,8 +12,8 @@ vcr.describe('Get group billing stats [UNIT]', function () {
         compute
             .groups()
             .getBillingStats({
-                dataCenter: DataCenter.DE_FRANKFURT,
-                name: Group.DEFAULT
+                dataCenter: compute.DataCenter.DE_FRANKFURT,
+                name: compute.Group.DEFAULT
             })
             .then(checkAsserts)
             .then(done);
