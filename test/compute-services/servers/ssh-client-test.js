00dbbb5f4cac8f5783c8bc39824822db54e6b6a1
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-08-07 17:40:24 +0300

    #154 Unit tests coverage improvement. try to pass ssh-client test

--- test/compute-services/servers/ssh-client-test.js
+++ test/compute-services/servers/ssh-client-test.js
@@ -7,34 +7,34 @@ var ServerBuilder = require('./../server-builder.js');
 
 describe('SSH client operation [INTEGRATION]', function () {
 
-    //var builder = new ServerBuilder(compute);
-    //
-    //it('Should execute commands through SSH', function (done) {
-    //    this.timeout(2 * 60 * 1000);
-    //
-    //    compute.servers()
-    //        .find({
-    //            dataCenter: compute.DataCenter.DE_FRANKFURT,
-    //            onlyActive: true
-    //        })
-    //        .then(function(result) {
-    //            if (result.length === 0) {
-    //                return builder.createCentOsVm({})
-    //                    .then(compute.servers().findSingle);
-    //            }
-    //            return _.sample(result, 2);
-    //        })
-    //        .then(function(servers) {
-    //            return compute.servers()
-    //                .execSsh(servers)
-    //                .run(['cd ~;ls', 'ls -l', 'ls -all']).then(function(result) {
-    //                    console.log(JSON.stringify(result, null, 2));
-    //                })
-    //                .run('ls -all').then(function(result) {
-    //                    console.log(JSON.stringify(result, null, 2));
-    //                });
-    //        })
-    //        .then(done);
-    //});
+    var builder = new ServerBuilder(compute);
+
+    it('Should execute commands through SSH', function (done) {
+        this.timeout(3 * 60 * 1000);
+
+        compute.servers()
+            .find({
+                dataCenter: compute.DataCenter.DE_FRANKFURT,
+                onlyActive: true
+            })
+            .then(function(result) {
+                if (result.length === 0) {
+                    return builder.createCentOsVm({})
+                        .then(compute.servers().findSingle);
+                }
+                return _.sample(result, 2);
+            })
+            .then(function(servers) {
+                return compute.servers()
+                    .execSsh(servers)
+                    .run(['cd ~;ls', 'ls -l', 'ls -all']).then(function(result) {
+                        console.log(JSON.stringify(result, null, 2));
+                    })
+                    .run('ls -all').then(function(result) {
+                        console.log(JSON.stringify(result, null, 2));
+                    });
+            })
+            .then(done);
+    });
 
 });
