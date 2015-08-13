07b9d742b32db7616c934e7ff2b07bbfc4efc3a7
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-08-13 15:41:32 +0300

    #156 Js docs. Refactoring. data centers functionality

--- test/compute-services/servers/find-available-imports-test.js
+++ test/compute-services/servers/find-available-imports-test.js
@@ -4,13 +4,13 @@ var TestAsserts = require("./../../test-asserts.js");
 
 describe('Find available server imports [UNIT]', function () {
     var compute = new Sdk('cloud_user', 'cloud_user_password').computeServices();
-    var templates = compute.templates();
+    var servers = compute.servers();
     var DataCenter = compute.DataCenter;
 
     vcr.it('Should found list of server imports', function (done) {
-        this.timeout(50 * 1000);
+        this.timeout(5 * 1000);
 
-        templates
+        servers
             .findAvailableServerImports(DataCenter.DE_FRANKFURT)
             .then(TestAsserts.assertNotNull)
             .then(function () {
