d7b22ff744a93c906238c6f138f6fdb1d9e083d4
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-09-01 19:52:25 +0300

    #159 Implement possibilities to add/remove secondary network for a server

---
+++ test/compute-services/servers/secondary-network-test.js
@@ -0,0 +1,64 @@
+
+var Sdk = require('./../../../lib/clc-sdk.js');
+
+var vcr = require('nock-vcr-recorder-mocha');
+var compute = new Sdk('cloud_user', 'cloud_user_password').computeServices();
+
+var _ = require('underscore');
+var assert = require('assert');
+
+vcr.describe('Secondary Networks Operations [UNIT]', function () {
+
+    var timeout = 10000;
+
+    var searchCriteria = {
+        dataCenter: compute.DataCenter.DE_FRANKFURT,
+        nameContains: 'cln'
+    };
+
+    var networkConfig = {
+        dataCenter: compute.DataCenter.DE_FRANKFURT,
+        name: "vlan_309_10.110.109"
+    };
+
+    it('Should add secondary network', function (done) {
+        this.timeout(timeout);
+
+        compute.servers()
+            .addSecondaryNetwork(searchCriteria, networkConfig)
+            .then(loadServerDetails)
+            .then(function(servers) {
+                _.each(servers, function(server) {
+                    assert.equal(server.details.secondaryIPAddresses.length, 1);
+                });
+            })
+            .then(done);
+    });
+
+    it('Should remove all secondary networks', function (done) {
+        this.timeout(timeout);
+
+        compute.servers()
+            .removeSecondaryNetwork(searchCriteria, networkConfig)
+            .then(loadServerDetails)
+            .then(function(servers) {
+                _.each(servers, serverHasNoSecondaryNetwork);
+            })
+            .then(done);
+    });
+
+    function serverHasNoSecondaryNetwork(server) {
+        assert(
+            _.isEmpty(
+                _.chain(server.details.secondaryIPAddresses)
+                    .value()
+            )
+        );
+    }
+
+    function loadServerDetails(serverRefs) {
+        assert(!_.isEmpty(serverRefs));
+
+        return compute.servers().find(serverRefs);
+    }
+});
