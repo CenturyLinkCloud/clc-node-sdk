8680f7a0bb6c9960f3954eb4f3f61c91b47691bd
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-10-02 12:32:12 +0300

    #171 Claim / Release / Update Networks. changes according comments

--- test/compute-services/networks/search-network-test.js
+++ test/compute-services/networks/search-network-test.js
@@ -1,13 +1,13 @@
 
 var vcr = require('nock-vcr-recorder-mocha');
 var Sdk = require('./../../../lib/clc-sdk.js');
-var compute = new Sdk(/*'cloud_user', 'cloud_user_password'*/).computeServices();
+var compute = new Sdk('cloud_user', 'cloud_user_password').computeServices();
 var assert = require('assert');
 var _ = require("underscore");
 
 vcr.describe('Search network operation [UNIT]', function () {
 
-    var timeout = 1000 * 1000;
+    var timeout = 10 * 1000;
 
     function assertThatSingleResultNotEmpty(result) {
         assert.equal(result.length, 1);
@@ -25,14 +25,16 @@ vcr.describe('Search network operation [UNIT]', function () {
     it('Should found network by id', function (done) {
         this.timeout(timeout);
 
+        var networkId = '34567c36e2bf47d6afbba207a3a28a5c';
+
         compute
             .networks()
             .find({
-                id: '34567c36e2bf47d6afbba207a3a28a5c'
+                id: networkId
             })
             .then(assertThatSingleResultNotEmpty)
             .then(function(network) {
-                assert.equal(network.id, '34567c36e2bf47d6afbba207a3a28a5c');
+                assert.equal(network.id, networkId);
             })
             .then(done);
     });
@@ -40,14 +42,16 @@ vcr.describe('Search network operation [UNIT]', function () {
     it('Should found network by name', function (done) {
         this.timeout(timeout);
 
+        var networkName = 'vlan_418_10.110.218';
+
         compute
             .networks()
             .find({
-                name: 'vlan_418_10.110.218'
+                name: networkName
             })
             .then(assertThatSingleResultNotEmpty)
             .then(function(network) {
-                assert.equal(network.name, 'vlan_418_10.110.218');
+                assert.equal(network.name, networkName);
             })
             .then(done);
     });
@@ -55,14 +59,16 @@ vcr.describe('Search network operation [UNIT]', function () {
     it('Should found network by gateway', function (done) {
         this.timeout(timeout);
 
+        var gateway = '10.110.37.1';
+
         compute
             .networks()
             .find({
-                gateway: '10.110.37.1'
+                gateway: gateway
             })
             .then(assertThatSingleResultNotEmpty)
             .then(function(network) {
-                assert.equal(network.gateway, '10.110.37.1');
+                assert.equal(network.gateway, gateway);
             })
             .then(done);
     });
@@ -70,15 +76,17 @@ vcr.describe('Search network operation [UNIT]', function () {
     it('Should found network by netmask', function (done) {
         this.timeout(timeout);
 
+        var netmask = '255.255.255.0';
+
         compute
             .networks()
             .find({
-                netmask: '255.255.255.0'
+                netmask: netmask
             })
             .then(assertThatResultNotEmpty)
             .then(function(networks) {
                 _.each(networks, function(network) {
-                    assert.equal(network.netmask, '255.255.255.0');
+                    assert.equal(network.netmask, netmask);
                 });
             })
             .then(done);
@@ -87,14 +95,16 @@ vcr.describe('Search network operation [UNIT]', function () {
     it('Should found network by vlan', function (done) {
         this.timeout(timeout);
 
+        var vlan = 719;
+
         compute
             .networks()
             .find({
-                vlan: 719
+                vlan: vlan
             })
             .then(assertThatSingleResultNotEmpty)
             .then(function(network) {
-                    assert.equal(network.vlan, 719);
+                    assert.equal(network.vlan, vlan);
             })
             .then(done);
     });
